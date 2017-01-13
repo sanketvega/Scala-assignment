1f67073af8cdfd1270b2de701df07ca66cc770a9
vandy <vandana.yadav759@gmail.com>
2017-01-13 18:53:34 +0530

    Submitting assignment 4th question

--- scala-2.11/Question4.scala
+++ scala-2.11/Question4.scala
@@ -1,6 +1,25 @@
 /**
-  * Created by nitin on 13/1/17.
+  * A program to sum of the digits of the product of numbers from 1 to n.
   */
-class Question4 {
+class Sumofdigits {    // class to find the sum  of digits of the product of numbers
+  var sum=0
+  val number = 7
+  var i = 1
+  var product = 1
 
+  while (i < number) {       // looping in to calculate the product
+      product = product * i
+    i=i+1
+  }
+  println(s"product = $product")
+  while(product>0) {        // looping in to calculate the sum of digits
+    var rem = product % 10
+    product=product/10
+    sum=sum+rem
+
+  }
+  println(s"sum = $sum")
+}
+object Question4 extends App {
+  val sumobj = new Sumofdigits()    //Instantiating the class Sumofdigits
 }
